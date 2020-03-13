5d8b827ff54e02c1154a8d97bea248a545c2cfd0
Thibault DUMAS <32924124+Thibalti@users.noreply.github.com>
2020-03-13 16:44:57 +0100

    test employecontroller

--- src/main/java/com/ipiecoles/java/java350/Controller/EmployeController.java
+++ src/main/java/com/ipiecoles/java/java350/Controller/EmployeController.java
@@ -8,14 +8,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/employes")
-public class EmployeController {/*
+public class EmployeController {
     @Autowired
     private EmployeRepository employeRepository;
 
     @GetMapping("{/id}")
     public Employe getEmploye(@PathVariable("id")Long id){
-        return employeRepository.findById(id).get();
-    }*/
+        Optional<Employe> optionalEmploye = employeRepository.findById(id);
+        if (optionalEmploye.isPresent()){
+            return optionalEmploye.get();
+        }
+        throw new EntityNotFoundException("Employe" + id + "introuvable");
+    }
 }
